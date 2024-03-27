5fb9be9d31a6a06eceafee7ae7e2aa29548e240e
Juan Pablo Guevara López <jpguevara909@soy.sena.edu.co>
2024-03-26 21:53:22 -0500

    Added documentation to init file

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+"""
+This file is used to initialize the Flask application and the database.
+The app object is created and configured with the config.py file.
+"""
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
