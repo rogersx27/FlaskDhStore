898750a2072c5ddbba7516768f28a6414ad9cafb
Juan Pablo Guevara López <jpguevara909@soy.sena.edu.co>
2024-03-31 13:38:00 -0500

    Add new methods to work with user login libraries

--- Models/Users.py
+++ Models/Users.py
@@ -3,14 +3,19 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
+# User session management for Flask
+from flask_login import UserMixin
+
+# Import the db object
 from __init__ import db
+
 # Import the models that have a relationship with the User
 from .Admins import Administrador
-from .Logistic import Logistico
 from .Clients import Cliente
+from .Logistic import Logistico
 
 
-class Usuario(db.Model):
+class Usuario(db.Model, UserMixin):
     """
     Modelo de la tabla usuarios
 
@@ -39,9 +44,25 @@ class Usuario(db.Model):
     pssword = Column(String(50))
     TOKEN = Column(String(250))
 
+    # Relationships
     administradores = relationship(Administrador, backref='usuarios', lazy=True)
     logisticos = relationship(Logistico, backref='usuarios', lazy=True)
     clientes = relationship(Cliente, backref='usuarios', lazy=True)
+
+    # Methods
+    def check_password(self, password):
+        return self.pssword == password
+    def get_id(self):
+        return self.id_user
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
 
     def __repr__(self):
         return f"<Usuario(id={self.id_user}, nombre={self.nombre}, apellido={self.apellido})>"
